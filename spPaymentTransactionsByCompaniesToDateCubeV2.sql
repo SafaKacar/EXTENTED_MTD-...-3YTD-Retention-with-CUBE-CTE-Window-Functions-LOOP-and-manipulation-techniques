8aab16f2adfe7774093dd0219c54bc751e3196a1
Safa Kaçar <61948895+SafaKacar@users.noreply.github.com>
2023-10-03 13:25:30 +0300

    Update spPaymentTransactionsByCompaniesToDateCubeV2.sql

--- spPaymentTransactionsByCompaniesToDateCubeV2.sql
+++ spPaymentTransactionsByCompaniesToDateCubeV2.sql
@@ -4,7 +4,12 @@ EXECUTE THIS QUERY FOR DAILY ETL PLAN:
 
 	DECLARE @DailySPDate AS DATE = CAST(GETDATE() AS DATE)
 	EXEC [DBO].[spPaymentTransactionsByCompaniesToDateCubeV2] @DailySPDate
-
+*** If you have daily process which can lock the stream, use WAITFOR like below to prevent it. ***
+E.g.
+GO 
+IF datepart(HH,getdate())>=0 AND datepart(HH,getdate())<4 BEGIN WAITFOR TIME '04:00'; END ELSE BEGIN exec [dbo].[spPaymentTransactionsByCompaniesToDateCubeV2] '2022-07-13'; END
+GO 
+*** *** ***
 --Note: You can use any date by replacing "@DailySPDate" with a date, e.g. 'yyyy-MM-dd' that included on the tables used in query
 ---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---
 SOME DETAILS :
@@ -19,6 +24,7 @@ SOME DETAILS :
 	2YTD = Last-2-Years-to-Date
 	3YTD = Last-3-Years-to-Date
 	FeatureLaunchedTD = All the time
+
 ---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---	---
 */
 USE DWH_Workspace
